a98f9134a193d7d8873ea01b0c9dee27e791587d
Duchicus <145117344+Duchicus@users.noreply.github.com>
2024-02-27 17:52:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Js-2-1-2-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Duchicus/Js-2-1-2-)
---
+++ script.js
@@ -0,0 +1,30 @@
+//Declara dos variables numéricas y realiza operaciones aritméticas con ellas (suma, resta, multiplicación, división, módulo) y muestra los resultados en la consola.
+
+let num1 = 2;
+let num2 = 3;
+
+let result = 0;
+
+result = num1 + num2;
+console.log(result);
+
+result = num1 - num2;
+console.log(result);
+
+result = num1 * num2;
+console.log(result);
+
+result = num1 / num2;
+console.log(result);
+
+//Realiza operaciones de comparación entre las variables y muestra los resultados en la consola.
+
+if (num1 < num2) {
+  console.log(true);
+}
+
+//Utiliza operadores lógicos para combinar condiciones y muestra los resultados en la consola.
+
+if (num1 - num2 === 0 || num1 + num2 === 0) {
+  console.log(true);
+}
